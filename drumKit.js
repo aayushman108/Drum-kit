a3d394498366219a605efea6f16de9ac8ae0960c
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-01-17 21:52:42 +0545

    still working

--- drumKit.js
+++ drumKit.js
@@ -1,10 +1,31 @@
-function myFunction(event){
-    const code= event.keycode;
-    const audio= document.querySelector(`audio[data-key="${event.keycode}"]`);
+function playSound(event){
+    const audio= document.querySelector(`audio[data-key="${event.keyCode}"]`);
     audio.currentTime=0; 
     audio.play();
+
+    const add= document.querySelector(`div[data-key="${event.keyCode}"]`).classList.add("enhance");
+    function myFunction(){
+    add.classList.remove("enhance");
+    }
+    setTimeout(myFunction, 200);
+    
 }
+window.addEventListener('keydown', playSound);
 
+/*function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+    var key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
-
-window.addEventListener('keydown', myFunction)
+    key.classList.add('playing');
+    
+    audio.currentTime = 0;
+    audio.play();
+  }
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  keys.forEach(key);
+  function key(value){
+    value.addEventListener('transitionend', removeTransition);
+  }
+  window.addEventListener('keydown', playSound);*/
