d4f0bc55e6c7cce8e87dbd636473cdf3d963b253
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-05-08 10:50:20 +0545

    transitionend eventListener was added

--- drumKit.js
+++ drumKit.js
@@ -4,28 +4,14 @@ function playSound(event){
     audio.play();
 
     const add= document.querySelector(`div[data-key="${event.keyCode}"]`).classList.add("enhance");
-    function myFunction(){
-    add.classList.remove("enhance");
-    }
-    setTimeout(myFunction, );
+    if(!audio) return;
     
 }
+const keys= Array.from(document.querySelectorAll(".key"));
+keys.forEach(key=> key.addEventListener("transitionend", removeTransition));
+
+function removeTransition(event){
+    event.target.classList.remove("enhance")
+}
+
 window.addEventListener('keydown', playSound);
-
-/*function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
-    var key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-
-    key.classList.add('playing');
-    
-    audio.currentTime = 0;
-    audio.play();
-  }
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  keys.forEach(key);
-  function key(value){
-    value.addEventListener('transitionend', removeTransition);
-  }
-  window.addEventListener('keydown', playSound);*/
